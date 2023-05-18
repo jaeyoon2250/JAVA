7128598934de3fe7e5d872985f79fa2107b730f8
jaeyoon2250 <jayoon2250@gmail.com>
2023-05-18 16:23:18 +0900

    과제1

---
+++ vending_machine.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class vending_machine {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        String cola, syder, hoanta, pepsi;
+        int[][] state = {
+            {500, 200, 800, 1000},
+            {15, 10, 2, 1}
+        };
+
+        System.out.println("")
+        scan.close();
+    }
+}
