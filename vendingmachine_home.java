2957d408a0caa998cb7b055dab4fd6ca5ffc372b
jaeyoon2250 <jayoon2250@gmail.com>
2023-05-25 16:19:57 +0900

    과제

---
+++ vendingmachine_home.java
@@ -0,0 +1,9 @@
+public class vendingmachine_home {
+
+        String[] menupan = { "콜라", "사이다", "환타", "펩시" };
+        int[][] state = {
+                { 500, 200, 800, 1000 },
+                { 15, 10, 2, 1 }
+        };
+        int input = 0, menu = 0;
+}
