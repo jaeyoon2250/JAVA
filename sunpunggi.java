5b406602aa43a1fd4414f782b73056983de6a5c1
jaeyoon2250 <jayoon2250@gmail.com>
2023-06-07 15:58:07 +0900

    선풍기과제

---
+++ sunpunggi.java
@@ -0,0 +1,11 @@
+public class sunpunggi {
+    //바람 세기, 모델명, 색상, 전원, 
+    String modelname;
+    String Color;
+    int powerlever;
+    
+
+    public void power(int OnOff){
+        
+    }
+}
---
+++ sunpunggi_work.java
@@ -0,0 +1,5 @@
+public class sunpunggi_work {
+    public static void main(String[] args){
+        
+    }
+}
