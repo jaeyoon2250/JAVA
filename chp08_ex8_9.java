1232b3978e66d57d198d2842b6d59ab46c78d047
jaeyoon2250 <jayoon2250@gmail.com>
2023-06-20 16:44:00 +0900

    1

---
+++ chp08_ex8_9.java
@@ -0,0 +1,29 @@
+public class chp08_ex8_9 {
+    public static void main(){
+        
+    }
+
+
+}
+
+class Coffee {
+    private String name;
+    private int price;
+
+    public Coffee(String n, int p) {
+        name = n;
+        price = p;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getprice() {
+        return price;
+    }
+
+    public void setPrice(int p) {
+        price = p;
+    }
+}
